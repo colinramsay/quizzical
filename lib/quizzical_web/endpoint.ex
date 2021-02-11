4d243abdbb5d39664236f6cc78997ac6eb39a2f6
Colin Ramsay <github@happyandlost.co.uk>
2021-02-11 16:56:21 +0000

    chore: prep for heroku

--- lib/quizzical_web/endpoint.ex
+++ lib/quizzical_web/endpoint.ex
@@ -11,7 +11,7 @@ defmodule QuizzicalWeb.Endpoint do
   ]
 
   socket "/socket", QuizzicalWeb.UserSocket,
-    websocket: true,
+    websocket: [timeout: 45_000],
     longpoll: false
 
   socket "/live", Phoenix.LiveView.Socket, websocket: [connect_info: [session: @session_options]]
