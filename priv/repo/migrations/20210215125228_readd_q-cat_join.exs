0b69a081966dea262e731330b44fab8ef16938df
Colin Ramsay <github@happyandlost.co.uk>
2021-02-15 12:53:05 +0000

    fix: migration

---
+++ priv/repo/migrations/20210215125228_readd_q-cat_join.exs
@@ -0,0 +1,10 @@
+defmodule :"Elixir.Quizzical.Repo.Migrations.ReaddQ-catJoin" do
+  use Ecto.Migration
+
+  def change do
+    create table(:questions_categories, primary_key: false) do
+      add :question_id, references(:questions, on_delete: :delete_all)
+      add :category_id, references(:categories, on_delete: :delete_all)
+    end
+  end
+end
